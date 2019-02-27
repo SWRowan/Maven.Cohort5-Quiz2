f0d54852a4e79ab1f5f9ec1ab66e05a9337ec971
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-02-27 15:56:00 -0500

    CheckPoint 2

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -60,3 +60,4 @@ public class ArrayUtility {
         return count;
     }
 }
+
--- src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
@@ -1,29 +1,71 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import com.j256.ormlite.stmt.query.In;
+
+import java.util.ArrayList;
 import java.util.List;
 
+
 public class ListUtility {
-    public Boolean add(int i) {
-        return null;
+    List<Integer> answer = new ArrayList<>();
+
+    public Boolean add(Integer i) {
+        this.answer = answer;
+        Integer baseLine = 0;
+        answer.add(i);
+        Integer size = answer.size();
+        return (baseLine != size);
     }
 
     public Integer size() {
-        return null;
+        this.answer = answer;
+
+        return answer.size();
     }
 
     public List<Integer> getUnique() {
+        this.answer = answer;
         return null;
     }
 
     public String join() {
+//        this.answer = answer;
+//        Integer[] quizTime = new Integer[answer.size()];
+//        quizTime = answer.toArray(quizTime);
+//        String str = quizTime.toString(str);
+//        System.out.println(str);
+//        //String answerString = str.join();
         return null;
     }
 
     public Integer mostCommon() {
-        return null;
+        this.answer = answer;
+        Integer[] quizTime = new Integer[answer.size()];
+        quizTime = answer.toArray(quizTime);
+        Integer count = 0;
+        Integer mostCommon = quizTime[0];
+        Integer secondCounter = 1;
+
+
+        for (int i = 0; i < quizTime.length; i++) {
+            Integer test = quizTime[i];
+            for (int j = 1; j < quizTime.length; j++) {
+                if (quizTime[j] == quizTime[j - 1]) {
+                    count++;
+                }
+
+                if (count == secondCounter) {
+                    mostCommon = test;
+                    secondCounter = count;
+                }
+            }
+
+        }
+        return mostCommon;
     }
 
     public Boolean contains(Integer valueToAdd) {
-        return null;
+        this.answer = answer;
+        return answer.contains(valueToAdd);
     }
 }
